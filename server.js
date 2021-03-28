65c9623499567472aa090d1363536271bf06a091
monther-alkhwaldeh <mmskbh.91@gmail.com>
2021-03-28 21:50:36 +0300

    edit2

--- server.js
+++ server.js
@@ -11,24 +11,23 @@ app.use(cors());
 app.get('/location', handelLocationRequest);
 app.get('/weather', handelWeatherRequest);
 
-
 function handelLocationRequest(req,res){
   const searchQuery = req.query.city;
   //   console.log(searchQuery);
   const locationsRawData = require('./data/location.json');
   const location = new LocationCity(locationsRawData[0],searchQuery);
   res.send(location);
-  console.log(location);
+//   console.log(location,searchQuery);
 }
 
 function handelWeatherRequest(req,res){
   const weatherArray=[];
-  const searchQuery = req.query.city;
+//   const searchQuery = req.query.city;
   const weatherRawData = require('./data/weather.json');
   weatherRawData.data.forEach(element => {
-    const weatherDescription=element.weather.description;
-    const dateTime=element.datetime;
-    let weatherData= new WeatherCity(element,searchQuery);
+    // const weatherDescription=element.weather.description;
+    // const dateTime=element.datetime;
+    let weatherData= new WeatherCity(element);
     // res.send(weatherData);
     weatherArray.push(weatherData);
   });
@@ -41,6 +40,7 @@ function handelWeatherRequest(req,res){
 function LocationCity (data,searchQuery){
   this.formatted_query = data.display_name;
   this.searchQuery= searchQuery;
+  this.city=data.city;
   this.latitude = data.lat;
   this.longitude = data.lon;
 
@@ -49,7 +49,7 @@ function WeatherCity(data,searchQuery){
   this.forcaste=data.weather.description;
   this.time=data.datetime;
 //   this.searchQuery=searchQuery;
-  console.log(this);
+//   console.log(this);
 
 }
 app.use('*', (req, res) => {
